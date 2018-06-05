c1968e9e7430b85789a741991731a3a899f03312
Pengcheng Li <lipengcheng1900@hotmail.com>
2018-06-05 19:52:05 +0800

    update codestyle

---
+++ Bitwise/eightQueensProblem.cpp
@@ -0,0 +1,46 @@
+// This program solves the problem of Eight Queens Puzzle using recursive backtracking algorithm
+// in bitwise operations.
+// 
+
+#include <cstdio>
+#include <ctime>
+
+static unsigned done = 1;
+static unsigned count = 0;
+
+void search(const unsigned column, const unsigned left_diagonal, const unsigned right_diagonal);
+
+int main()
+{
+	size_t chessboard_size;
+	printf("Please input the chessboard size:");
+	scanf("%zd", &chessboard_size);
+	--(done <<= chessboard_size);
+
+	printf("%zd queens problem\n", chessboard_size);
+	clock_t start = clock();
+	search(0, 0, 0);
+	clock_t finish = clock();
+	double duration = (double)((finish - start) / CLOCKS_PER_SEC);
+	printf("Find %d solutions in %lf clocks\n", count, duration);
+
+	return 0;
+}
+
+void search(const unsigned column, const unsigned left_diagonal, const unsigned right_diagonal)
+{	
+	if (column != done)
+	{
+		int state = done & ~(column | left_diagonal | right_diagonal);
+		while (state)
+		{
+			const unsigned position = state & -state;
+			state -= position;
+			search(column + position, (left_diagonal + position) << 1, (right_diagonal + position) >> 1);
+		}
+	}
+	else
+	{
+		++count;
+	}
+}
--- Simplified/eightQueensProblem.c
+++ Simplified/eightQueensProblem.c
@@ -1,9 +1,9 @@
-/* This program solves the problem of Eight Queens Puzzle using recursive backtracking algorithm
- */
+// This program solves the problem of Eight Queens Puzzle using recursive backtracking algorithm
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define N 8
 
@@ -15,9 +15,14 @@ void search(const unsigned row);
 
 int main() 
 {
+	clock_t start, finish;
+
 	printf("%d queens problem:\n", N);
+	start = clock();
 	search(0);
-	printf("%d soluntions!", count);
+	finish = clock();
+	printf("Find %d soluntions in %ld clocks", count, finish - start);
+
 	return 0;
 }
 
@@ -35,17 +40,19 @@ bool check(const unsigned row)
 
 void search(const unsigned row) 
 {
-	if (row >= N) 
+	if (row < N) 
+	{
+		for (position[row] = 0; position[row] < N; position[row]++)
+		{
+			if (check(row))
+			{
+				search(row + 1);
+			}
+		}
+	}
+	else
 	{
 		++count;
-		return;
-	}
-	for (position[row] = 0; position[row] < N; position[row]++)
-	{
-		if (check(row))
-		{
-			search(row + 1);
-		}
 	}
 }
 
